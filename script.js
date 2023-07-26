55db51007f1a6217b6283ac0ea6b44c3c1d1bada
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-07-26 17:11:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 // Your code here.
-// test.spec.js
-describe('Click and Drag Test', () => {
-  it('should slide to subsequent cubes on click and drag', () => {
-    cy.visit('/main.html');
-
-    // Simulate the click and drag behavior
-    cy.get('.items')
-      .trigger('mousedown', { which: 1, pageX: 493, pageY: 391, force: true })
-      .trigger('mousemove', { which: 1, pageX: 271, pageY: 391, force: true })
-      .trigger('mouseup', { force: true });
-
-    // Check if the container has scrolled to the right
-    cy.get('.items').should('have.prop', 'scrollLeft').and('be.greaterThan', 0);
-  });
-});
